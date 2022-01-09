a01187e8b22fb8b4d48276f72177154963dae6da
na1xpro <na1xpro>
2022-01-10 01:58:32 +0300

    rewrite outlook_bot.py (1.improved driver path)(2.Rewritten code)

--- outlook_bot.py
+++ outlook_bot.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import os.path
-import warnings
 
-warnings.filterwarnings("ignore") #Отключение варнингов
+
+
 # Перепатч хрома на загрузку файлов в нужную нам папку, а именно в корневой каталог проекта в папку downloaded_files
 
-driver_path = os.getcwd() + '/webdriver/chromedriver.exe'
+driver_path = Service(os.getcwd() + '/webdriver/chromedriver.exe')
 
 options = Options()
 options.add_argument('--window-size=1400,800')
@@ -19,29 +21,30 @@ options.add_experimental_option('prefs', {
     'download.directory_upgrade': True,
 })
 
-driver_login = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+
+driver_login  = webdriver.Chrome(service=driver_path,options=options)
 
 driver_login.get('https://login.live.com/')
 
 # Вход в акаунт
-email = driver_login.find_element_by_name("loginfmt")
+email = driver_login.find_element(By.NAME,"loginfmt")
 email.send_keys("vivexpro@outlook.com")
 
 
 def dalie():
-    knopka_dalie = driver_login.find_element_by_id("idSIButton9").click()
+    knopka_dalie = driver_login.find_element(By.ID,"idSIButton9").click()
 
 
 dalie()
 sleep(1)
-password = driver_login.find_element_by_name("passwd")
+password = driver_login.find_element(By.NAME,"passwd")
 password.send_keys("Vivexpass1")
 sleep(1)
 dalie()
 
-knopka_bolshe_ne_pokazivat = driver_login.find_element_by_id("KmsiCheckboxField").click()
+knopka_bolshe_ne_pokazivat = driver_login.find_element(By.ID,"KmsiCheckboxField").click()
 
-knopka_net = driver_login.find_element_by_id("idBtn_Back").click()
+knopka_net = driver_login.find_element(By.ID,"idBtn_Back").click()
 print("------------------------Вход в акаунт прошёл успешно!------------------------")
 
 # Переход на почту  и выбор собщения
@@ -54,25 +57,25 @@ def download_button():  # Скачка собщения
     sleep(5)
 
     print("Скачка собщения----------")
-    download_message = driver_login.find_element_by_xpath('//button[@name = "Download"]').click()
+    download_message = driver_login.find_element(By.XPATH,'//button[@name = "Download"]').click()
     print("------------------------Скачивание  собщения прошло успешно!------------------------")
-    close_form_download = driver_login.find_element_by_xpath('//button[@title = "Close" ]').click()
+    close_form_download = driver_login.find_element(By.XPATH,'//button[@title = "Close" ]').click()
 
 
-enter_trainingA = driver_login.find_element_by_xpath('//div[@title="Training A"]').click()  # Скачка А
+enter_trainingA = driver_login.find_element(By.XPATH,'//div[@title="Training A"]').click()  # Скачка А
 sleep(3)
-enter_message = driver_login.find_element_by_xpath('//div[@title = "Training A.xlsx"]').click()
+enter_message = driver_login.find_element(By.XPATH,'//div[@title = "Training A.xlsx"]').click()
 print("------------------------Выбор собщения прошёл успешно!------------------------")
 download_button()
 
-enter_trainingB = driver_login.find_element_by_xpath('//div[@title="Training B"]').click()  # Скачка B
+enter_trainingB = driver_login.find_element(By.XPATH,'//div[@title="Training B"]').click()  # Скачка B
 sleep(3)
-enter_message = driver_login.find_element_by_xpath('//div[@title = "Training B.xlsx"]').click()
+enter_message = driver_login.find_element(By.XPATH,'//div[@title = "Training B.xlsx"]').click()
 download_button()
 
-enter_trainingC = driver_login.find_element_by_xpath('//div[@title="Training C"]').click()  # Скачка C
+enter_trainingC = driver_login.find_element(By.XPATH,'//div[@title="Training C"]').click()  # Скачка C
 sleep(3)
-enter_message = driver_login.find_element_by_xpath('//div[@title = "Training C.xlsx"]').click()
+enter_message = driver_login.find_element(By.XPATH,'//div[@title = "Training C.xlsx"]').click()
 download_button()
 
 # Парсинг собщения из файл
@@ -92,24 +95,24 @@ sleep(4)
 
 for email in mail_list:
     print("-----------------Создание Собщения!-----------------")
-    open_message_icon = driver_login.find_element_by_xpath('//span[text()="New message"]').click()
+    open_message_icon = driver_login.find_element(By.XPATH,'//span[text()="New message"]').click()
     sleep(5)
     print("-----------------Добавление юзеров!-----------------")
-    users_message = driver_login.find_element_by_xpath('//input[@aria-label = "To"]')
+    users_message = driver_login.find_element(By.XPATH,'//input[@aria-label = "To"]')
     users_message.send_keys(email)
 
     sleep(3)
     print("-----------------СозданиЕ Темы!-----------------")
-    title_message = driver_login.find_element_by_xpath('//input[@aria-label = "Add a subject"]')
+    title_message = driver_login.find_element(By.XPATH,'//input[@aria-label = "Add a subject"]')
 
     title_message.send_keys('You need to pass a training')
 
     print("-----------------Создание самого собщения!-----------------")
-    message_for_users = driver_login.find_element_by_xpath('//div[@aria-label="Message body"]')
+    message_for_users = driver_login.find_element(By.XPATH,'//div[@aria-label="Message body"]')
     message_for_users.send_keys('Hello! You need to pass trainings. Have a nice day!')
 
     print("-----------------Отправка Собщения!-----------------")
-    message_send = driver_login.find_element_by_xpath('//button[contains(@title, "Send")]').click()
+    message_send = driver_login.find_element(By.XPATH,'//button[contains(@title, "Send")]').click()
     print("----------------- Собщение Отправлено!-----------------")
 
 sleep(5)
