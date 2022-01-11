ce62c40f6d394eb3200653df86b2ad61395e7701
evgeny.ravlyuk <yevhenii.ravliuk@jetsoftpro.com>
2022-01-11 21:15:23 +0200

    first init

--- webdriver/driver.py
+++ webdriver/driver.py
@@ -5,21 +5,24 @@ from sys import platform
 import os
 
 if "linux" in platform:
-    path_driver = '/webdriver/chromedriver'
+    path_driver = "/webdriver/chromedriver"
 elif platform == "win32":
-    path_driver = r'\webdriver\chromedriver.exe'
+    path_driver = r"\webdriver\chromedriver.exe"
 
 way = os.getcwd() + path_driver
 os.chmod(way, 755)
 driver_service = Service(way)
 
 options = Options()
-options.add_argument('--window-size=1400,800')
-options.add_experimental_option('prefs', {
-    'download.default_directory': os.getcwd() + r'\downloaded_files',
-    'download.prompt_for_download': False,
-    'safebrowsing.enabled': True,
-    'download.directory_upgrade': True,
-})
+options.add_argument("--window-size=1400,800")
+options.add_experimental_option(
+    "prefs",
+    {
+        "download.default_directory": os.getcwd() + r"\downloaded_files",
+        "download.prompt_for_download": False,
+        "safebrowsing.enabled": True,
+        "download.directory_upgrade": True,
+    },
+)
 
 driver = webdriver.Chrome(service=driver_service, options=options)
