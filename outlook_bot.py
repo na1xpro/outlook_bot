77160bf741dd8ab5f430ef06cf636c8c8055cd4e
na1xpro <na1xpro>
2022-01-12 20:55:25 +0300

    remove outlook_bot

--- outlook_bot.py
+++ outlook_bot.py
@@ -1,67 +1,67 @@
 import os
 from time import sleep
 import pandas as pd
-
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver.driver import driver
 from constants import credentials
 
 driver.get('https://login.live.com/')
 
 # Вход в акаунт
-driver.find_element(By.NAME, "loginfmt").send_keys(credentials['login'])
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.NAME, "loginfmt"))).send_keys(credentials['login'])
 
 
 def dalie():
-    driver.find_element(By.ID, "idSIButton9").click()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "idSIButton9"))).click()
 
 
 dalie()
-sleep(1)
-password = driver.find_element(By.NAME, "passwd").send_keys(credentials['password'])
-sleep(1)
-dalie()
-# Тест строка
-driver.find_element(By.ID, "KmsiCheckboxField").click()
 
-driver.find_element(By.ID, "idBtn_Back").click()
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.NAME, "passwd"))).send_keys(credentials['password'])
+
+dalie()
+
+
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "KmsiCheckboxField"))).click()
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "idBtn_Back"))).click()
+
 print("------------------------Вход в акаунт прошёл успешно!------------------------")
 
 # Переход на почту  и выбор собщения
 
 driver.get('https://outlook.live.com/mail/')
-sleep(4)
 
 
 def download_button():  # Скачка собщения
-    sleep(5)
-
     print("Скачка собщения----------")
-    driver.find_element(By.XPATH, '//button[@name = "Download"]').click()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//button[@name = "Download"]'))).click()
     print("------------------------Скачивание  собщения прошло успешно!------------------------")
-    driver.find_element(By.XPATH, '//button[@title = "Close" ]').click()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//button[@title = "Close" ]'))).click()
 
 
-driver.find_element(By.XPATH, '//div[@title="Training A"]').click()  # Скачка А
-sleep(3)
-driver.find_element(By.XPATH, '//div[@title = "Training A.xlsx"]').click()
+# Скач# Скачка А
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training A"]'))).click()
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title = "Training A.xlsx"]'))).click()
 print("------------------------Выбор собщения прошёл успешно!------------------------")
 download_button()
+# Скачка B
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training B"]'))).click()
 
-driver.find_element(By.XPATH, '//div[@title="Training B"]').click()  # Скачка B
-sleep(3)
-driver.find_element(By.XPATH, '//div[@title = "Training B.xlsx"]').click()
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title = "Training B.xlsx"]'))).click()
+
 download_button()
 
-driver.find_element(By.XPATH, '//div[@title="Training C"]').click()  # Скачка C
-sleep(3)
-enter_message = driver.find_element(By.XPATH, '//div[@title = "Training C.xlsx"]').click()
+
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training C"]'))).click()
+WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title = "Training C.xlsx"]'))).click()
+
 download_button()
 
 # Парсинг собщения из файл
 print("------------------------Парсинг  собщения!------------------------")
-sleep(3)
+
 list_bad = []
 for f in [os.getcwd() + '/downloaded_files/Training A.xlsx',
           os.getcwd() + '/downloaded_files/Training B.xlsx',
@@ -71,27 +71,18 @@ for f in [os.getcwd() + '/downloaded_files/Training A.xlsx',
     list_bad.extend(mail)
     mail_list = list(set(list_bad))
 print("------------------------Парсинг  собщения прошёл успешно!------------------------")
-
-sleep(4)
-
 for email in mail_list:
     print("-----------------Создание Собщения!-----------------")
-    open_message_icon = driver.find_element(By.XPATH, '//span[text()="New message"]').click()
-    sleep(5)
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//span[text()="New message"]'))).click()
     print("-----------------Добавление юзеров!-----------------")
-    users_message = driver.find_element(By.XPATH, '//input[@aria-label = "To"]').send_keys(email)
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "To"]'))).send_keys(email)
 
-    sleep(3)
     print("-----------------СозданиЕ Темы!-----------------")
-    title_message = driver.find_element(By.XPATH, '//input[@aria-label = "Add a subject"]')
-
-    title_message.send_keys('You need to pass a training')
-
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "Add a subject"]'))).send_keys('You need to pass a training')
     print("-----------------Создание самого собщения!-----------------")
-    message_for_users = driver.find_element(By.XPATH, '//div[@aria-label="Message body"]').send_keys(
-        'Hello! You need to pass trainings. Have a nice day!')
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@aria-label="Message body"]'))).send_keys('Hello! You need to pass trainings. Have a nice day!')
     print("-----------------Отправка Собщения!-----------------")
-    message_send = driver.find_element(By.XPATH, '//button[contains(@title, "Send")]').click()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//button[contains(@title, "Send")]'))).click()
     print("----------------- Собщение Отправлено!-----------------")
 
 sleep(5)
