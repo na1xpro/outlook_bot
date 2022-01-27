8d6b38a0a0b91411ca268037e62d4e244ca8b3d8
na1xpro <na1xpro>
2022-01-27 18:46:49 +0300

    All code has been broken down into functions for easier understanding

--- outlook_bot.py
+++ outlook_bot.py
@@ -14,86 +14,99 @@ from constants import os_path
 
 driver.get('https://login.live.com/')
 
+
 # Вход в акаунт
-WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="i0116"]'))).send_keys(
-    credentials['login'])
+def auth():
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="i0116"]'))).send_keys(
+        credentials['login'])
+
+    def dalie():
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//input[@id="idSIButton9"]'))).click()
+
+    dalie()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="i0118"]'))).send_keys(
+        credentials['password'])
+    dalie()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="idBtn_Back"]'))).click()
+    logger.info("Account authorization successful.")
+    driver.get('https://outlook.live.com/mail/')
 
 
-def dalie():
-    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="idSIButton9"]'))).click()
+auth()
 
 
-dalie()
-
-WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="i0118"]'))).send_keys(
-    credentials['password'])
-
-dalie()
-
-WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//input[@id="idBtn_Back"]'))).click()
-
-logger.info("Account authorization successful.")
-
-# Переход на почту  и выбор собщения
-
-driver.get('https://outlook.live.com/mail/')
-
-logger.info('Check for the presence of an existing file in a folder.')
-logger.warning('The files in the folder may be deleted!')
-pas = os.getcwd() + os_path[platform]['path_download_folder']
-files = glob.glob(pas + r'\*.xlsx')
-for filename in files:
-    os.unlink(filename)
+def checking_files_in_folder():
+    logger.info('Check for the presence of an existing file in a folder.')
+    logger.warning('The files in the folder may be deleted!')
+    pas = os.getcwd() + os_path[platform]['path_download_folder']
+    global files
+    files = glob.glob(pas + r'\*.xlsx')
+    for filename in files:
+        os.unlink(filename)
+    return files
 
 
-def download_button():
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//div[@class = "_24WqHp8mfxSp2QIJMkmSrM"]'))).click()
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, "//button[@title = 'More actions']"))).click()
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, "//i[@data-icon-name = 'Download']"))).click()
+checking_files_in_folder()
 
 
-WebDriverWait(driver, 13).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training A"]'))).click()
+def download_message():
+    def download_button():
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//div[@class = "_24WqHp8mfxSp2QIJMkmSrM"]'))).click()
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, "//button[@title = 'More actions']"))).click()
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, "//i[@data-icon-name = 'Download']"))).click()
 
-download_button()
-
-WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training B"]'))).click()
-
-download_button()
-
-WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training C"]'))).click()
-
-download_button()
-
-logger.info('Parsing a message...')
-list_bad = []
-for file in files:
-    data = pd.read_excel(file)
-    mail = data['Mail'].tolist()
-    list_bad.extend(mail)
-    mail_list = list(set(list_bad))
+    WebDriverWait(driver, 13).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training A"]'))).click()
+    download_button()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training B"]'))).click()
+    download_button()
+    WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//div[@title="Training C"]'))).click()
+    download_button()
 
 
-logger.info('Parsing of the message was successful!')
+download_message()
 
-for email in mail_list:
-    logger.info("Create a message!")
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//span[text()="New message"]'))).click()
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "To"]'))).send_keys(email)
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "Add a subject"]'))).send_keys(
-        put_message['title_message'])
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//div[@aria-label="Message body"]'))).send_keys(
-        put_message['message'])
-    WebDriverWait(driver, 10).until(
-        ec.visibility_of_element_located((By.XPATH, '//button[contains(@title, "Send")]'))).click()
-    logger.info('Message sent!')
 
+# Парсинг собщенимя
+def parsing_message():
+    logger.info('Parsing a message...')
+    list_bad = []
+    for file in files:
+        data = pd.read_excel(file)
+        mail = data['Mail'].tolist()
+        list_bad.extend(mail)
+        global mail_list
+        mail_list = list(set(list_bad))
+        return mail_list
+
+    logger.info('Parsing of the message was successful!')
+
+
+parsing_message()
+
+
+def send_message():
+    for email in parsing_message():
+        logger.info("Create a message!")
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//span[text()="New message"]'))).click()
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "To"]'))).send_keys(email)
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//input[@aria-label = "Add a subject"]'))).send_keys(
+            put_message['title_message'])
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//div[@aria-label="Message body"]'))).send_keys(
+            put_message['message'])
+        WebDriverWait(driver, 10).until(
+            ec.visibility_of_element_located((By.XPATH, '//button[contains(@title, "Send")]'))).click()
+        logger.info('Message sent!')
+
+
+send_message()
 sleep(3)
 driver.close()
 driver.quit()
